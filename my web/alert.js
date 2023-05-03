c411d0738b799e6329d7d0776d183282f925352e
itzzmeAron <128341720+itzzmeAron@users.noreply.github.com>
2023-05-03 13:34:23 +0800

    Add files via upload

---
+++ my web/alert.js
@@ -0,0 +1,8 @@
+// Get the button element
+const alertBtn = document.getElementById('alert-btn');
+
+// Add a click event listener to the button
+alertBtn.addEventListener('click', function() {
+  // Show an alert box with a message
+  alert('You clicked the button!');
+});
